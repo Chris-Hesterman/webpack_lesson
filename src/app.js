d7b6050b203b873cf889c8ce1bf1d0bbdf523994
Chris Hesterman <spoiler14@comcast.net>
2018-04-02 13:04:22 -0700

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const template = <p>Here is some JSX</p>;
+
+
+ReactDOM.render(template, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+//entry --> output
+const path = require("path");
+
+
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    }
+};
